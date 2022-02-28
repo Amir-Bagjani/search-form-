e2d468e1187f7451d415a2207d874e5bf7540b1e
amirbagjni <amirbagjani@gmail.com>
2022-02-28 22:45:23 +0330

    approch two/2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import Table from "./Table";
 import { Users } from "./users"
 
+const keys = ["first_name", "last_name", "email"]
+
 function App() {
   const [query,setQuery] = useState("")
 
   const search = (data) => {
-    return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query))
+    // return data.filter(item => item.first_name.toLowerCase().includes(query) || item.last_name.toLowerCase().includes(query) || item.email.toLowerCase().includes(query))
+    return data.filter(item => keys.some(key => item[key].toLowerCase().includes(query)))
   }
 
   return (
