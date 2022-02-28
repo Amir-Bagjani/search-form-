de47413a3fc0baf52501af580aa157f439bf02b0
amirbagjni <amirbagjani@gmail.com>
2022-02-28 23:30:45 +0330

    initial server side

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Users } from "./users"
+import axios from "axios"
+
+
 
 function App() {
   const [query,setQuery] = useState("")
+  const [data,setData] = useState([])
+
+
+  useEffect(() => {
+
+    const fetchData = async() => {
+      const res = await axios.get(`http://localhost:4040/users`)
+      setData(res.data)
+    }
+
+    fetchData()
+
+  }, [query])
+
+
+
+
   return (
     <div className="App">
       <input className="input" type="search" onChange={e => setQuery(e.target.value)} />
